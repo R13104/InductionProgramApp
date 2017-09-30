e627175ee91f8553efb7994b64972f05914f94d1
yugi003 <yugantar.kumar@elucidata.io>
2017-10-01 00:44:21 +0530

    ftr: basic setup for routing

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,17 +3,28 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import {
+  FrontendResourceComponent,
+  PageNotFoundComponent,
+  WelcomeComponent,
+} from './components'
 
 import { AppMaterialModule } from './shared';
 
+import { routing } from './app.routing';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrontendResourceComponent,
+    PageNotFoundComponent,
+    WelcomeComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppMaterialModule,
+    routing,
   ],
   providers: [],
   bootstrap: [AppComponent]
