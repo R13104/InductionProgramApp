7b82cc9018d5a818066f85a9469de5076dac010f
yugi003 <yugantar.kumar@elucidata.io>
2017-10-01 00:43:41 +0530

    ftr: add new component to introduce routing module

---
+++ my-app/src/app/components/welcome/welcome.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'welcome',
+    styleUrls: ['./welcome.component.scss'],
+    templateUrl: './welcome.component.html'
+})
+
+export class WelcomeComponent {
+    
+}
